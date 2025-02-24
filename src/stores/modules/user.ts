6b819683f9e7e0f5ebf363a2c832792f238b4448
Sakura <1430008132@qq.com>
2025-02-24 18:28:42 +0800

    设置token

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -1,13 +1,14 @@
 /*
  * @Author: V_JNNJIELU-PCGP\v_jnnjieluo v_jnnjieluo@tencent.com
  * @Date: 2025-01-13 18:54:19
- * @LastEditors: V_JNNJIELU-PCGP\v_jnnjieluo v_jnnjieluo@tencent.com
- * @LastEditTime: 2025-01-16 17:42:38
- * @FilePath: \Vue3-ts\src\stores\modules\user.ts
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-02-24 18:24:48
+ * @FilePath: \Vue3-TS\src\stores\modules\user.ts
  * @Description:
  */
 import { defineStore } from 'pinia'
 import { userState } from '@/stores/interface'
+import axios from '@/api/axios'
 
 export const useUserStore = defineStore({
 	id: 'sora-user',
@@ -19,6 +20,7 @@ export const useUserStore = defineStore({
 	actions: {
 		setToken(token: string) {
 			this.token = token
+			axios.defaults.headers.common['token'] = token
 		}
 	}
 })
